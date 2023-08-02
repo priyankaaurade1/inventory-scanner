fdf8d2033cb851917a3eb3b2a915e1f5ad281706
priyankaaurade1 <70333636+priyankaaurade1@users.noreply.github.com>
2023-08-02 13:06:24 +0100

    1st commit

---
+++ dashboard/urls.py
@@ -0,0 +1,14 @@
+from . import views
+from django.urls import path,include
+from django.conf import settings
+from django.contrib.auth.views import LogoutView
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+path('',views.index, name="index"),  
+# path('login/' , views.render_login, name="login"),
+# path("logout/", LogoutView.as_view(), name="logout"),
+# path('register/', views.render_register, name='register'),
+
+]
