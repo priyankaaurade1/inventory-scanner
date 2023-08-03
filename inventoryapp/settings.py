107850abad78b0eb5f499f25ce0c3db8152821c8
sarvesh <salam.sarvesh@gmail.com>
2023-08-03 23:15:04 +0530

    added pwa settings

--- inventoryapp/settings.py
+++ inventoryapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vjc))7%_)5=t(#v3btkw5-g517uh0%vm0+0k#22d$_!1*d6t+i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,3 +124,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SERVICE_WORKER_URL = '/static/assets/js/service-worker.js'
