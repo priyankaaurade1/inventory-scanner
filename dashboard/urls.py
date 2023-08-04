5e77c13c414aa2f21ad3562ca4c2a6d6e5df4443
Forms9 <104549595+Forms9@users.noreply.github.com>
2023-08-04 16:23:03 +0530

    profile page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 path('',views.index, name="index"),  
+ path('user_profile/',views.user_profile, name='user_profile'),
 # path('login/' , views.render_login, name="login"),
 # path("logout/", LogoutView.as_view(), name="logout"),
 # path('register/', views.render_register, name='register'),
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def user_profile(request):
+    return render(request, 'users-profile.html')
